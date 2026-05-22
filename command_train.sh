5c73fb6a817e46d7a39962eec930a13199265358
Daniel-Dainj <1784910993@qq.com>
2026-05-22 22:00:46 +0800

    格式化脚本

--- command_train.sh
+++ command_train.sh
@@ -1,1 +1,8 @@
-CUDA_VISIBLE_DEVICES=4 python train.py --camera kinect --log_dir logs/log_kn --batch_size 4 --learning_rate 0.001 --model_name minkuresunet --dataset_root /data3/graspnet
+CUDA_VISIBLE_DEVICES=0 \
+uv run python train.py \
+  --camera realsense \
+  --log_dir logs/log_rs \
+  --batch_size 4 \
+  --learning_rate 0.001 \
+  --model_name minkuresunet \
+  --dataset_root data/graspnet
